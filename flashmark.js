7b3053fc96c5576cf635e508d4266c217ce6186d
Jan Kropiwnicki <jan.kropiwnicki@infotech.edu.pl>
2023-12-19 20:18:51 +0100

    Minified the file

--- flashmark.js
+++ flashmark.js
@@ -2,7 +2,16 @@ import('https://cdn.jsdelivr.net/npm/marked/marked.min.js').then(() => {
     console.log('marked loaded')
     console.log(document.getElementsByTagName('markdown'))
     for (let x of document.getElementsByTagName('markdown')) {
-        x.innerHTML = marked.marked(x.innerHTML.replace(/[^\S\r\n]+/g, ' '))
-        x.classList.add('rendered')
+        if (x.hasAttribute('src')) {
+            fetch(x.getAttribute('src')).then((response) => {
+                return response.text()
+            }).then((text) => {
+                x.innerHTML = marked.marked(text.replace(/[^\S\r\n]+/g, ' '))
+                x.classList.add('rendered')
+            })
+        } else {
+            x.innerHTML = marked.marked(x.innerHTML.replace(/[^\S\r\n]+/g, ' '))
+            x.classList.add('rendered')
+        }
     }
 })
